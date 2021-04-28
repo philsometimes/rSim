e083f785f02bfe9d7181c184fe77c4a20c7444ef
Phil Fahn-Lai <philsometimes@gmail.com>
2021-04-27 20:16:58 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+/** @jsxImportSource theme-ui */
+import Simulation from "./Simulate";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Simulation />
+    </div>
+  );
+}
---
+++ src/Simulate.js
@@ -0,0 +1,290 @@
+/** @jsxImportSource theme-ui */
+import { useEffect, useState } from "react";
+import * as math from "mathjs";
+
+// const UI = () => {
+//   const
+// }
+
+const Simulation = () => {
+  const rX = math.unit("0.1 m");
+  const rY = math.unit("0.1 m");
+  const limbL = math.unit("0.1 m");
+  const shoulderOffsetTheta = math.unit(45, "deg");
+  const adductionTheta = math.unit(0, "deg"); //in degrees, from horizontal
+  const grfTheta = math.unit(0, "deg"); //deg
+  const zoom = 1000;
+
+  //constants and units
+  const gravity = math.gravity;
+  const tissueDensity = math.unit("997 kg / m^3");
+  const newtonPixScale = 10; //1 Newton equals 10 pixels
+  const specificTension = math.unit("200 kPa"); //200 kPa (https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4968477/) = 200000 N m^2
+  const aspectRatio = math.divide(rX, rY);
+  const xSectionA = math.multiply(math.pi, rX, rY);
+  const circleEquivalentR = math.sqrt(math.divide(xSectionA, math.pi));
+  const volume = math.multiply(xSectionA, circleEquivalentR);
+  const mass = math.multiply(tissueDensity, volume);
+  const weight = math.multiply(mass, gravity);
+  const grfY = math.divide(weight, -4);
+  const grfX = math.multiply(grfY, math.tan(grfTheta));
+  const grf = [grfX, grfY];
+  const momentArm = 2;
+  const moment = 34;
+  // const moment = math.cross(momentArm, grf);
+
+  // conversions
+  const rXpx = rX.toNumber("mm");
+  const rYpx = rY.toNumber("mm");
+  const limbLpx = limbL.toNumber("mm");
+
+  // positioning
+  const zero = { x: zoom / 2, y: zoom / 2 };
+  const shoulder = {
+    x: zero.x + rXpx * math.cos(shoulderOffsetTheta),
+    y: zero.y + rYpx * math.sin(shoulderOffsetTheta),
+    ghostX: zero.x - rXpx * math.cos(shoulderOffsetTheta)
+  };
+  const elbow = {
+    x: shoulder.x + limbLpx * math.cos(adductionTheta),
+    y: shoulder.y + limbLpx * math.sin(adductionTheta),
+    ghostX: shoulder.ghostX - limbLpx * math.cos(adductionTheta)
+  };
+  const wrist = {
+    x: elbow.x,
+    y: elbow.y + limbLpx,
+    ghostX: elbow.ghostX
+  };
+
+  return (
+    <div sx={{ height: "100vh", width: "100vw" }}>
+      <svg viewBox={`0 0 ${zoom} ${zoom}`}>
+        <defs>
+          <marker
+            id="arrowHeadOrange"
+            viewBox="0 0 10 10"
+            refX="1"
+            refY="5"
+            markerUnits="strokeWidth"
+            markerWidth="4"
+            markerHeight="5"
+            orient="auto"
+          >
+            <path d="M 0 0 L 10 5 L 0 10 z" fill="orange" />
+          </marker>
+          <marker
+            id="arrowHeadGreen"
+            viewBox="0 0 10 10"
+            refX="1"
+            refY="5"
+            markerUnits="strokeWidth"
+            markerWidth="4"
+            markerHeight="5"
+            orient="auto"
+          >
+            <path d="M 0 0 L 10 5 L 0 10 z" fill="lightgreen" />
+          </marker>
+        </defs>
+        <Torso zero={zero} rXpx={rXpx} rYpx={rYpx} />
+        <LiveLimb
+          shoulder={shoulder}
+          elbow={elbow}
+          wrist={wrist}
+          limbL={limbL}
+        />
+        <GhostLimb shoulder={shoulder} elbow={elbow} wrist={wrist} />
+        <Forces shoulder={shoulder} wrist={wrist} grf={grf} moment={moment} />
+        <Labels
+          zero={zero}
+          shoulder={shoulder}
+          elbow={elbow}
+          wrist={wrist}
+          rX={rX}
+          rY={rY}
+          mass={mass}
+          limbL={limbL}
+        />
+      </svg>
+    </div>
+  );
+};
+
+const Forces = ({ shoulder, wrist, grf, moment }) => {
+  const grfMag = math.sqrt(math.add(math.square(grf[0]), math.square(grf[1])));
+  const x1 = wrist.x;
+  const y1 = wrist.y;
+  const x2 = wrist.x + grf[0].toNumeric("dN");
+  const y2 = wrist.y + grf[1].toNumeric("dN");
+  return (
+    <>
+      <line
+        x1={x1}
+        y1={y1}
+        x2={x2}
+        y2={y2}
+        markerEnd="url(#arrowHeadOrange)"
+        stroke="orange"
+        strokeWidth="3"
+      />
+      <text x={`${wrist.x * 1.02}`} y={y2} fill="orange">
+        {grfMag.format({ notation: "fixed", precision: 2 })}
+      </text>
+      <path
+        d={`M ${shoulder.x + 20} ${shoulder.y} a 20 20 0 0 1 -40 0
+           a 20 20 0 0 1 20 -20`}
+        markerEnd="url(#arrowHeadGreen)"
+        stroke="lightgreen"
+        strokeWidth="3"
+        fill="none"
+      />
+      <text x={`${shoulder.x + 25}`} y={`${shoulder.y + 25}`} fill="lightgreen">
+        {moment.toFixed(2)} Nm
+      </text>
+    </>
+  );
+};
+
+const Labels = ({ zero, shoulder, elbow, wrist, rX, rY, mass, limbL }) => {
+  console.log(mass);
+  return (
+    <>
+      <text
+        x={`${zero.x}`}
+        y={`${zero.y + rY.toNumeric("mm") / 2}`}
+        textAnchor="middle"
+      >
+        {mass.format({ notation: "fixed", precision: 2 })}
+      </text>
+      <text
+        x={`${zero.x + rX.toNumeric("mm") / 2}`}
+        y={`${zero.y * 0.98}`}
+        textAnchor="middle"
+      >
+        {rX.value} m
+      </text>
+      <text
+        x={`${zero.x * 1.02}`}
+        y={`${zero.y - rY.toNumeric("mm") / 2}`}
+        textAnchor="start"
+      >
+        {rY.value} m
+      </text>
+      <text
+        x={`${shoulder.x + limbL.toNumeric("mm") / 2}`}
+        y={`${shoulder.y * 0.98}`}
+        textAnchor="middle"
+      >
+        {limbL.value} m
+      </text>
+      <text
+        x={`${elbow.x * 1.02}`}
+        y={`${elbow.y + limbL.toNumeric("mm") / 2}`}
+        textAnchor="start"
+      >
+        {limbL.value} m
+      </text>
+    </>
+  );
+};
+
+const Torso = ({ zero, rXpx, rYpx }) => {
+  return (
+    <>
+      <ellipse
+        cx={`${zero.x}`}
+        cy={`${zero.y}`}
+        rx={`${rXpx}`}
+        ry={`${rYpx}`}
+        fill="pink"
+      />
+      <circle cx={`${zero.x}`} cy={`${zero.y}`} r="5" fill="black" />
+      <line
+        x1={`${zero.x}`}
+        y1={`${zero.y}`}
+        x2={`${zero.x + rXpx}`}
+        y2={`${zero.y}`}
+        stroke="black"
+        strokeWidth="1"
+        strokeDasharray="3,5"
+      />
+      <line
+        x1={`${zero.x}`}
+        y1={`${zero.y}`}
+        x2={`${zero.x}`}
+        y2={`${zero.y - rYpx}`}
+        stroke="black"
+        strokeWidth="1"
+        strokeDasharray="3,5"
+      />
+    </>
+  );
+};
+
+const GhostLimb = ({ shoulder, elbow, wrist }) => {
+  return (
+    <>
+      <line
+        x1={`${shoulder.ghostX}`}
+        y1={`${shoulder.y}`}
+        x2={`${elbow.ghostX}`}
+        y2={`${elbow.y}`}
+        stroke="#EAEAEA"
+        strokeWidth="10"
+        strokeLinecap="round"
+      />
+      <line
+        x1={`${elbow.ghostX}`}
+        y1={`${elbow.y}`}
+        x2={`${wrist.ghostX}`}
+        y2={`${wrist.y}`}
+        stroke="#EAEAEA"
+        strokeWidth="10"
+        strokeLinecap="round"
+      />
+    </>
+  );
+};
+
+const LiveLimb = ({ shoulder, elbow, wrist, limbL }) => {
+  return (
+    <>
+      <line
+        x1={`${shoulder.x}`}
+        y1={`${shoulder.y}`}
+        x2={`${elbow.x}`}
+        y2={`${elbow.y}`}
+        stroke="black"
+        strokeWidth="10"
+      />
+      <line
+        x1={`${elbow.x}`}
+        y1={`${elbow.y}`}
+        x2={`${wrist.x}`}
+        y2={`${wrist.y}`}
+        stroke="black"
+        strokeWidth="10"
+      />
+      <circle
+        cx={`${shoulder.x}`}
+        cy={`${shoulder.y}`}
+        r="8"
+        stroke="black"
+        strokeWidth="2"
+        fill="white"
+      />
+      <circle cx={`${shoulder.x}`} cy={`${shoulder.y}`} r="4" fill="black" />
+      <circle
+        cx={`${elbow.x}`}
+        cy={`${elbow.y}`}
+        r="8"
+        stroke="black"
+        strokeWidth="2"
+        fill="white"
+      />
+      <circle cx={`${elbow.x}`} cy={`${elbow.y}`} r="4" fill="black" />
+      <circle cx={`${wrist.x}`} cy={`${wrist.y}`} r="5" fill="black" />
+    </>
+  );
+};
+
+export default Simulation;
